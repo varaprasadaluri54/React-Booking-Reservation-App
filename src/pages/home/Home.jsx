3deead0267ea0fde3f5453a2198378e98da4e25e
Aluri varaprasad <38999815+varaprasadaluri54@users.noreply.github.com>
2022-04-30 22:11:09 +0530

    mail

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,7 @@
 import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Header from "../../components/header/Header";
+import MailList from "../../components/mailList/MailList";
 import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
@@ -13,6 +15,9 @@ export default function Home() {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
       </div>
     </div>
   );
